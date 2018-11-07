a24c48e8b0820cc74fec1d75d1887a5846590816
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-11-07 22:04:33 +0000

    Moving the ball

--- 01_pong/main.lua
+++ 01_pong/main.lua
@@ -2,10 +2,11 @@ function love.load()
     ballX = love.graphics.getWidth() / 2
     ballY = love.graphics.getHeight() / 2
     ballRad = 5
+    ballSpeed = 100
 end
 
 function love.update(dt)
-
+    ballX = ballX + ballSpeed * dt
 end
 
 function love.draw()
