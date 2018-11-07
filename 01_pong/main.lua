ee3d3ffaab31f9b408ede7198313fdce0714ecbe
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-11-07 22:27:04 +0000

    Confine the ball to the screen

--- 01_pong/main.lua
+++ 01_pong/main.lua
@@ -7,8 +7,17 @@ end
 
 function love.update(dt)
     ballX = ballX + ballSpeed * dt
+    if ballX < 0 then
+        ballX = 0
+        ballSpeed = -ballSpeed
+    end
+    if ballX > love.graphics.getWidth() then
+        ballX = love.graphics.getWidth()
+        ballSpeed = -ballSpeed
+    end
 end
 
 function love.draw()
-    love.graphics.circle('fill', ballX, ballY, ballRad)
+    love.graphics.circle("fill", ballX, ballY, ballRad)
+    -- love.graphics.print(string.format("%.0f", ballX), 10, 10)
 end
