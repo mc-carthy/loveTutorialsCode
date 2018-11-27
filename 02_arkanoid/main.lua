b258388c0614529f2b8b512d6d6449abb682bf7b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-11-27 21:52:14 +0000

    Adding bricks

--- 02_arkanoid/main.lua
+++ 02_arkanoid/main.lua
@@ -1,10 +1,25 @@
+paddle = {}
+bricks = {}
+brick = {}
+
 function love.load()
-    paddle = {}
     paddle.w = 100
     paddle.h = 10
     paddle.y = love.graphics.getHeight() - 100
     paddle.x = love.graphics.getWidth() / 2 - paddle.w / 2
     paddle.speed = 400
+
+    brick.w = 80
+    brick.h = 20
+    
+    bricks.rows = 4
+    bricks.columns = 7
+    bricks.paddingX = 10
+    bricks.paddingY = 5
+    bricks.originX = 80
+    bricks.originY = 40
+    bricks.levelbricks = {}
+    bricks.createbricks()
 end
 
 function love.update(dt)
@@ -21,11 +36,36 @@ end
 
 function love.draw()
     love.graphics.rectangle("fill", paddle.x, paddle.y, paddle.w, paddle.h)
+    bricks.drawbricks()
 end
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
+    end
+end
+
+function bricks.createbricks()
+    for x = 1, bricks.columns do
+        for y = 1, bricks.rows do
+            local b = bricks.createBrick(bricks.originX + (brick.w + bricks.paddingX) * (x - 1), bricks.originY + (brick.h + bricks.paddingY) * (y - 1))
+            table.insert(bricks.levelbricks, b)
+        end
+    end
+end
+
+function bricks.createBrick(x, y, w, h)
+    local b = {}
+    b.x = x
+    b.y = y
+    b.w = w or brick.w
+    b.h = h or brick.h
+    return b
+end
+
+function bricks.drawbricks()
+    for _, b in pairs(bricks.levelbricks) do
+        love.graphics.rectangle("line", b.x, b.y, brick.w, brick.h)
     end
 end
 
